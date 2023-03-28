8f423f0fa6278e52a4d2e432a578e2d26db95ae0
Dilara Uluturhan <dilarauluturhan@outlook.com>
2023-03-28 13:47:15 +0300

    Homepage created

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,7 +2,34 @@ import React from 'react';
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="d-flex justify-content-center mt-5">
+      <div className="col-md-6">
+        <div className="card">
+          <div className="card-body">
+            <form>
+              <div className="form-group">
+                <textarea className="form-control" rows="5" placeholder="What do you think?"></textarea>
+              </div>
+              <div className="form-group mt-2">
+                <button className="btn btn-primary" style={{ float: "right" }}>Share</button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="card mt-3">
+          <div className="card-body">
+            <h5>Dilara Uluturhan - 28.03.2023 21:51</h5>
+            <p>Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet</p>
+          </div>
+        </div>
+        <div className="card mt-3">
+          <div className="card-body">
+            <h5>Dilara Uluturhan - 28.03.2023 21:51</h5>
+            <p>Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
